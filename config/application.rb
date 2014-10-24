a1603ba4e2c40e0e72d1e26aaf1921b07bdb220d
shaynarang <shaynarang@gmail.com>
2014-10-24 14:12:53 -0500

    update asset config

--- config/application.rb
+++ config/application.rb
@@ -19,10 +19,13 @@ module Heathens
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
     config.generators do |g|
       g.template_engine :haml
       g.assets          false
       g.helper          false
     end
+
+    config.serve_static_assets = true
   end
 end
