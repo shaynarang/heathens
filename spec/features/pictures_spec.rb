885f7fe73204087e9707f8f0e845a6e71a27e228
shaynarang <shaynarang@gmail.com>
2014-10-30 19:00:01 -0500

    add fancybox to pictures page

--- spec/features/pictures_spec.rb
+++ spec/features/pictures_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe "pictures" do
   before(:each) do
     first_image = FactoryGirl.create(:image)
     second_image = FactoryGirl.create(:image, content: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'photos', 'test2.jpg')))
-    visit "/"
     gallery = FactoryGirl.create(:gallery)
     gallery.images << first_image
     gallery.images << second_image
+    visit "/"
     find("li > a > img#pictures_icon").click
   end
     
@@ -15,5 +15,12 @@ RSpec.describe "pictures" do
     expect(page).to have_content("PICTURES")
     expect(page).to have_selector("img[alt=\"Medium test\"]")
     expect(page).to have_selector("img[alt=\"Medium test2\"]")
+  end
+
+  it "displays images via fancybox when clicked", :js => true do
+    expect(page).to have_selector(".fancybox > img[src*='medium_test']")
+    expect(page).to_not have_selector("#fancybox-img[src*='large_test']")
+    find("img[alt=\"Medium test\"]").click
+    expect(page).to have_selector("#fancybox-img[src*='large_test']")
   end
 end
