dddf9e0f2582a72353bb7e045760150fb5fce71a
shaynarang <shaynarang@gmail.com>
2015-04-01 16:44:22 -0500

    revert last commit, provide error feedback via flash notice

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,10 +11,10 @@ class MessagesController < ApplicationController
       MessageMailer.new_message(@message).deliver
       notice = "Thank you for contacting us. We will be in touch."
     else
-      notice = nil
+      notice = "Something went wrong. Please try again."
     end
 
-    flash[:notice] = notice if notice
+    flash[:notice] = notice
 
     respond_to do |format|
       format.js { render "create", :locals => { section: section } }
