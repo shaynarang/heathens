c8852f2ab76f1bc2269c6098968d499b95c7b26e
shaynarang <shaynarang@gmail.com>
2015-04-11 16:15:47 -0500

    default landing to words partial

--- app/assets/javascripts/script.js.coffee
+++ app/assets/javascripts/script.js.coffee
@@ -3,9 +3,14 @@ $ ->
     $("#standard_nav nav ul li a").removeClass "highlight"
     $("#mobile_nav nav #expanded ul li a").removeClass "highlight"
     $(element).addClass "highlight"
+
+  highlight $("a#words_link")
     
   $("#standard_nav nav ul li a").click ->
     highlight this
 
   $("#mobile_nav nav #expanded ul li a").click ->
     highlight this
+
+  $("#heading_row").click ->
+    highlight $("a#words_link")
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
-    # @random_image_url = Image.random.content.large.url
+    @posts = Post.order('created_at DESC').all
+    @categories = @posts.pluck(:categories).uniq.flatten
   end
 
   def display_content
-    # @random_image_url = Image.random.content.large.url
     @message = Message.new
     @galleries = Gallery.all
     @posts = Post.order('created_at DESC').all
