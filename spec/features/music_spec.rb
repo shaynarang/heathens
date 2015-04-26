1b27578781be7ac85f68114dae099d1e5816fc3e
shaynarang <shaynarang@gmail.com>
2015-04-26 18:21:14 -0500

    update music spec

--- spec/features/music_spec.rb
+++ spec/features/music_spec.rb
@@ -4,11 +4,10 @@ RSpec.describe "music" do
   before(:each) do
     FactoryGirl.create(:image)
     visit "/"
-    find("li > a#music_link").click
+    find("#standard_nav > nav > ul > li > a#music_link").click
   end
     
-  it "populates the content section with music content", :js => true do
-    content = "Vivamus elit nisl, placerat eget placerat non, porta sed justo. Maecenas at convallis erat, at vulputate quam. Nullam sagittis est quis facilisis bibendum. Morbi ut massa eu sem vestibulum fringilla nec sed velit. Sed a enim iaculis, volutpat urna id, iaculis justo. Nulla non nisl auctor, ultricies eros eget, semper mi. Duis sit amet justo tortor."
-    expect(page).to have_content(content.upcase)
+  it "redirects to the bandcamp page" do
+    expect(current_url).to eq("http://www.hthns.bandcamp.com/")
   end
 end
