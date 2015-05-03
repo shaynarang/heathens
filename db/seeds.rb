fd8243ab9915433c82d7add9619be7ef88aaca46
shaynarang <shaynarang@gmail.com>
2015-05-03 18:30:07 -0500

    update seeds, style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
-promo_gallery = Gallery.create(name: "Promo Images") unless Gallery.find_by_name("Promo Images")
+# DISABLE IMAGE CREATION FOR NOW
 
-Dir.foreach('app/assets/images/promo') do |file_name|
-  next if file_name == '.' or file_name == '..' or file_name == ".DS_Store"
-  promo_image = File.open("app/assets/images/promo/#{file_name}")
-  promo_gallery.images.create(content: promo_image) unless Image.find_by_content(file_name)
-end
+# promo_gallery = Gallery.create(name: "Promo Images") unless Gallery.find_by_name("Promo Images")
+
+# Dir.foreach('app/assets/images/promo') do |file_name|
+#   next if file_name == '.' or file_name == '..' or file_name == ".DS_Store"
+#   promo_image = File.open("app/assets/images/promo/#{file_name}")
+#   promo_gallery.images.create(content: promo_image) unless Image.find_by_content(file_name)
+# end
 
 Category.destroy_all
 
@@ -16,37 +18,20 @@ categories = [
   {name: "Other"}
 ]
 
-news_posts = [
-  {author: "Shay", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec risus mattis, suscipit magna eget, suscipit augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam aliquam commodo lacus in varius. In ac magna nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec ullamcorper ipsum nec tellus maximus cursus. Etiam et metus facilisis, suscipit tellus quis, rhoncus dolor. Etiam consectetur sed purus lobortis pellentesque. Curabitur a sollicitudin libero, ut efficitur dui.", title: "Dolor sit" },
-  {author: "Gor", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec risus mattis, suscipit magna eget, suscipit augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam aliquam commodo lacus in varius. In ac magna nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec ullamcorper ipsum nec tellus maximus cursus. Etiam et metus facilisis, suscipit tellus quis, rhoncus dolor. Etiam consectetur sed purus lobortis pellentesque. Curabitur a sollicitudin libero, ut efficitur dui.", title: "Lorem ipsum" }
-]
-
-review_posts = [
-  {author: "Shay", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec risus mattis, suscipit magna eget, suscipit augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam aliquam commodo lacus in varius. In ac magna nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec ullamcorper ipsum nec tellus maximus cursus. Etiam et metus facilisis, suscipit tellus quis, rhoncus dolor. Etiam consectetur sed purus lobortis pellentesque. Curabitur a sollicitudin libero, ut efficitur dui.", title: "Consectetur adipiscing elit"},
-  {author: "Gor", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec risus mattis, suscipit magna eget, suscipit augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam aliquam commodo lacus in varius. In ac magna nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec ullamcorper ipsum nec tellus maximus cursus. Etiam et metus facilisis, suscipit tellus quis, rhoncus dolor. Etiam consectetur sed purus lobortis pellentesque. Curabitur a sollicitudin libero, ut efficitur dui.", title: "Praesent nec"}
-]
-
-other_posts = [
-  {author: "Shay", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec risus mattis, suscipit magna eget, suscipit augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam aliquam commodo lacus in varius. In ac magna nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec ullamcorper ipsum nec tellus maximus cursus. Etiam et metus facilisis, suscipit tellus quis, rhoncus dolor. Etiam consectetur sed purus lobortis pellentesque. Curabitur a sollicitudin libero, ut efficitur dui.", title: "Risus mattis" },
-  {author: "Gor", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec risus mattis, suscipit magna eget, suscipit augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam aliquam commodo lacus in varius. In ac magna nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec ullamcorper ipsum nec tellus maximus cursus. Etiam et metus facilisis, suscipit tellus quis, rhoncus dolor. Etiam consectetur sed purus lobortis pellentesque. Curabitur a sollicitudin libero, ut efficitur dui.", title: "Suscipit magna eget"},
-  {author: "Shay", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec risus mattis, suscipit magna eget, suscipit augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam aliquam commodo lacus in varius. In ac magna nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec ullamcorper ipsum nec tellus maximus cursus. Etiam et metus facilisis, suscipit tellus quis, rhoncus dolor. Etiam consectetur sed purus lobortis pellentesque. Curabitur a sollicitudin libero, ut efficitur dui.", title: "Suscipit augue" }
+posts = [
+  {author: "Shay", content: "<p>We're always late to the party. That statement bares relevance to multiple areas of our lives, and literally being late to parties. Blame it on bad time management. Blame it on tentativeness. Blame it on a sundry assortment of inexcusable excuses. Whatever the case, we're here now and we're ready to get down. Is there any beer left? How about juice? Gor likes juice.</p><p>This area of the site is dedicated to words. The posts herein may involve 'stream of consciousness' babble, record reviews, HTHNS news, and really whatever Gor and I respectively deem (in)appropriate. After all, we've got a name to live up to. Keep your eyes peeled.</p>", title: "Late to the Party", category: "News"},
+  {author: "Shay", content: "<p>HTHNS has been a work in progress for the past eighteen months. It would look/sound very different if it weren't for <a href='http://www.vitalmastering.com/' target='_blank'>Jack Braglia</a> whose mastering prowess and patience helped improve GRTST HTS tremendously and <a href='http://www.zachkeller.net' target='_blank'>Zach Keller</a> whose artwork and design makes it challenging for the music to live up to. Our utmost appreciation to those gents.</p><p>GRTST HTS is set for release later this month.</p>", title: "Credits", category: "News"}
 ]
 
 categories.each do |category|
   Category.create(category)
 end
 
-news_posts.each do |post|
-  category = Category.where(name: "News").first
-  category.posts << Post.create(post)
-end
-
-review_posts.each do |post|
-  category = Category.where(name: "Review").first
-  category.posts << Post.create(post)
-end
-
-other_posts.each do |post|
-  category = Category.where(name: "Other").first
-  category.posts << Post.create(post)
+posts.each do |post|
+  author = post[:author]
+  content = post[:content]
+  title = post[:title]
+  category = post[:category]
+  category = Category.where(name: category).first
+  category.posts << Post.create(author: author, content: content, title: title)
 end
